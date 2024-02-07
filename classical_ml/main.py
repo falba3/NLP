e9e579a4936b4114ee020a209b6785772a0b7bc9
falba3 <99144557+falba3@users.noreply.github.com>
2024-02-07 01:12:27 +0100

    Feb 7 - Script Running

--- classical_ml/main.py
+++ classical_ml/main.py
@@ -1,38 +1,80 @@
+#!/usr/bin/env python3
 
 import pandas as pd
+from datasets import load_dataset
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import f1_score
-import pickle
 
-def get_score_from_model(model):
 
-    # Loading the test data
-    test = pd.read_csv('data/test.parquet')
+# Loading the dataset and additional data (from imdb)
+df = load_dataset('rotten_tomatoes')
+imdb = load_dataset('imdb')
 
-    # Splitting test data
-    X_test = test['text']
-    y_test = test['label']
+# Splitting train data
+X_train = df['train']['text']
+y_train = df['train']['label']
 
-    preds = model.predict(X_test)
+# Additional IMDB data
+X_train += imdb['train']['text']
+y_train += imdb['train']['label']
+
+# Splitting test data
+X_test = df['test']['text']
+y_test = df['test']['label']
+
+
+# Fitting the model with best parameters that achieved a high score
+tfidf_lr = Pipeline(
+    steps=[
+        ("vectorizer", TfidfVectorizer(
+            max_features=50000,
+            ngram_range=(1, 3),
+            sublinear_tf=True,
+            smooth_idf=True)),
+
+        ("model", LogisticRegression(
+            C=1,
+            penalty='l2',
+            solver='saga'))
+    ])
+
+tfidf_lr.fit(X_train, y_train)
+
+
+def get_score_from_model(fitted_model):
+    global df, X_test, y_test
+
+    preds = fitted_model.predict(X_test)
     score = round(f1_score(y_test, preds), 4)
 
     return score
 
-def make_preds(model, out_file_name = "results.csv"):
-    model.predict()
 
-    # Loading the test data
-    test = pd.read_csv('data/test.parquet')
+def make_preds(fitted_model):
+    global df, X_test, y_test
 
-    # Splitting test data
-    X_test = test['text']
-    y_test = test['label']
+    preds = fitted_model.predict(X_test)
+    preds_df = pd.DataFrame(preds, columns = ['pred'])
+    index_df = pd.DataFrame(list(range(0, len(preds))), columns=['index'])
+    preds = pd.concat([index_df, preds_df], axis = 1)
 
-    preds = model.predict(X_test)
-    pd.DataFrame(preds)
     return preds
 
 
 def main():
-    score = get_score_from_model('models/tfidf_lr_81.pkl')
+    global df, X_test, y_test, tfidf_lr
 
+    # Getting and printing the f1 score
+    score = get_score_from_model(tfidf_lr)
     print(score)
+
+    # Getting predictions and saving into a csv
+    preds = make_preds(fitted_model=tfidf_lr)
+    preds.to_csv('results.csv')
+
+
+if __name__ == "__main__":
+    main()
