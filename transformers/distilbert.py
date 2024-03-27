42da507b89365a8a63a47281f047e33d78718b89
falba3 <99144557+falba3@users.noreply.github.com>
2024-03-27 20:33:38 +0100

    Mar 27 - Distilbert Trials

--- transformers/distilbert.py
+++ transformers/distilbert.py
@@ -71,7 +71,7 @@ def main():
         "learning_rate": [1e-5, 2e-5],
         "per_device_train_batch_size": [16, 32],
         "per_device_eval_batch_size": [16, 32],
-        "num_train_epochs": [10],
+        "num_train_epochs": [5],
         "weight_decay": [0.01, 0.001],
         # "fp16": [True, False],
         "load_best_model_at_end": [True],
