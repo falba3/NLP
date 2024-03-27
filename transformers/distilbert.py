36d88c3edb44e77ee0a7c170f7bb828e0b7f9ee0
falba3 <99144557+falba3@users.noreply.github.com>
2024-03-27 18:43:27 +0100

    Mar 27 - Distilbert Trials

---
+++ transformers/distilbert.py
@@ -0,0 +1,145 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification, TrainingArguments, Trainer, TrainerCallback
+from sklearn.metrics import f1_score
+import numpy as np
+import torch
+from itertools import product #
+
+
+torch.manual_seed(0) # to replicate results
+dataset = load_dataset('rotten_tomatoes')
+
+# Customized Stopping Callback
+class CustomEarlyStoppingCallback(TrainerCallback):
+    """
+    Stopping Callback that will stop training based on f1 metric
+    default patience: 3 epochs
+    default delta: 0.01 increase  in f1
+    """
+    def __init__(self, delta=0.01, patience=3, metric_name="f1"):
+        self.delta = delta
+        self.patience = patience
+        self.metric_name = metric_name
+        self.best_metric = None
+        self.wait = 0
+
+    def on_evaluate(self, args, state, control, **kwargs):
+        current_metric = state.metrics.get(self.metric_name)
+        if current_metric is None:
+            return
+        if self.best_metric is None:
+            self.best_metric = current_metric
+        elif current_metric - self.best_metric > self.delta:
+            self.best_metric = current_metric
+            self.wait = 0
+        else:
+            self.wait += 1
+            if self.wait >= self.patience:
+                control.should_training_stop = True
+
+
+def main():
+    """
+    This main function will use the 'distilbert-base-uncased' model checkpoint
+    to train on the rotten tomatoes dataset.
+    """
+
+    # Defining model checkpoint and batch size
+    model_checkpoint = "distilbert-base-uncased"
+
+    # loading the tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+
+    # Preparing Labels
+    label2id = {lab: i for i, lab in enumerate(dataset["train"].features["label"].names)}
+
+    # Tokenizing our sentences
+    def preprocess_function(samples):
+        return tokenizer(samples["text"], padding="longest", truncation=True)
+    tokenized_dataset = dataset.map(preprocess_function, batched=True, num_proc=2)
+
+    # Model Setup (Configuration and Labels)
+    config = AutoConfig.from_pretrained(model_checkpoint, num_labels=len(label2id), id2label={i: lab for lab, i in label2id.items()})
+    config.label2id = label2id
+    model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, config=config)
+
+    param_grid = {
+        "output_dir": ["distilbert-rottentomatoes"],
+        "report_to": ["none"],
+        "evaluation_strategy": ["epoch"],
+        "save_strategy": ["epoch"],
+        "learning_rate": [1e-5, 2e-5],
+        "per_device_train_batch_size": [16, 32],
+        "per_device_eval_batch_size": [16, 32],
+        "num_train_epochs": [10],
+        "weight_decay": [0.01, 0.001],
+        "fp16": [True, False],
+        "load_best_model_at_end": [True],
+        "metric_for_best_model": ["f1"]
+    }
+
+
+    # Function for computing macro f1 metric
+    def compute_metrics(pred):
+        predictions, labels = pred
+        predictions = np.argmax(predictions, axis=1)
+
+        res = f1_score(labels, predictions, average="macro")
+        return {"f1": res}
+
+    # Instantiating the custom early stopping callback
+    custom_early_stopping = CustomEarlyStoppingCallback(delta=0.01, patience=3, metric_name="f1")
+
+    def tuner(parameter_grid):
+        best_score = 0
+        best_params = None
+        best_trainer = None
+
+        # Generate all combinations of parameter values
+        param_combinations = product(*parameter_grid.values())
+
+        for params in param_combinations:
+            # Initialize a new Trainer object with the given parameters
+            print(f"TRAINING PARAMETERS: {params}")
+            args = TrainingArguments(**params)
+            trainer = Trainer(
+                model=model,
+                args=args,
+                train_dataset=tokenized_dataset["train"],
+                eval_dataset=tokenized_dataset["validation"],
+                tokenizer=tokenizer,
+                compute_metrics=compute_metrics,
+                callbacks=[custom_early_stopping]
+            )
+
+            # Train the model
+            trainer.train()
+
+            # Evaluate the model on validation set
+            results = trainer.evaluate()
+
+            # Check if the current combination is the best
+            if results['eval_f1'] > best_score:
+                best_score = results['eval_f1']
+                best_params = params
+                best_trainer = trainer
+
+        # # Set the best parameters to the trainer
+        # for param_name, param_value in zip(parameter_grid.keys(), best_params):
+        #     setattr(trainer.args, param_name, param_value)
+
+        # Evaluate the model on the test set using the best parameters
+        test_results = best_trainer.evaluate(tokenized_dataset["test"])
+        print(f"f1 test score: {round(test_results, 4)}\nbest parameters: {best_params}")
+
+        # return best_params, best_score, test_results
+
+    # results = trainer.evaluate(tokenized_dataset["test"])
+    # print(f"f1 score: {round(results['eval_f1'], 4)}")
+
+    tuner(parameter_grid=param_grid)
+
+
+if __name__ == "__main__":
+    main()
+
--- transformers/distilbert_save.py
+++ transformers/distilbert_save.py
@@ -3,6 +3,7 @@ from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassifi
 from sklearn.metrics import f1_score
 import numpy as np
 import torch
+from itertools import product #
 
 
 torch.manual_seed(0) # to replicate results
@@ -39,12 +40,12 @@ class CustomEarlyStoppingCallback(TrainerCallback):
 
 def main():
     """
-    DOCSTRING
+    This main function will use the 'distilbert-base-uncased' model checkpoint
+    to train on the rotten tomatoes dataset.
     """
 
     # Defining model checkpoint and batch size
     model_checkpoint = "distilbert-base-uncased"
-    batch_size = 16
 
     # loading the tokenizer
     tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
@@ -62,6 +63,20 @@ def main():
     config.label2id = label2id
     model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, config=config)
 
+    param_grid = {
+        "output_dir": ["distilbert-rottentomatoes"],
+        "report_to": ["none"],
+        "evaluation_strategy": ["epoch"],
+        "save_strategy": ["epoch"],
+        "learning_rate": [1e-5, 2e-5],
+        "per_device_train_batch_size": [16, 32],
+        "per_device_eval_batch_size": [16, 32],
+        "num_train_epochs": [10],
+        "weight_decay": [0.01, 0.001],
+        "fp16": [True, False],
+        "load_best_model_at_end": [True],
+        "metric_for_best_model": ["f1"]
+    }
 
     # Training Arguments (to tune)
     args = TrainingArguments(
@@ -70,16 +85,16 @@ def main():
         report_to="none",  # don't report to tensorboard or wandb
         save_strategy="epoch",
         learning_rate=2e-5,  # learning rate to use in Adam Optimizer.
-        per_device_train_batch_size=batch_size,  # size of the batch for forward pass.
-        per_device_eval_batch_size=batch_size,  # "" for eval.
-        num_train_epochs=5,  # total number of training epochs
+        per_device_train_batch_size=16,  # size of the batch for forward pass.
+        per_device_eval_batch_size=16,  # "" for eval.
+        num_train_epochs=10,  # total number of training epochs
         weight_decay=0.01,  # parameter for adam optimizer
         load_best_model_at_end=True, # whether to load the best performing model at the end (best checkpoint, at the best performing moment in training.)
         metric_for_best_model="f1",  # metric for choosing the best model.
         # fp16=True # for mixed precision (lower memory usage)
     )
 
-    # Computing Metrics
+    # Function for computing macro f1 metric
     def compute_metrics(pred):
         predictions, labels = pred
         predictions = np.argmax(predictions, axis=1)
@@ -87,9 +102,10 @@ def main():
         res = f1_score(labels, predictions, average="macro")
         return {"f1": res}
 
-    # Creating the trainer to fine-tune our model over our data
+    # Instantiating the custom early stopping callback
     custom_early_stopping = CustomEarlyStoppingCallback(delta=0.01, patience=3, metric_name="f1")
 
+    # Creating the trainer to fine-tune our model over our data
     trainer = Trainer(
         model,
         args,
@@ -101,11 +117,45 @@ def main():
     )
 
     # Training the trainer
-    trainer.train()
+    # trainer.train()
 
-    # Evaluating our results on the test
-    results = trainer.evaluate(tokenized_dataset["test"])
-    print(f"f1 score: {round(results['eval_f1'], 4)}")
+    def tuner(trainer, parameter_grid, test_dataset):
+        best_score = float('-inf')
+        best_params = None
+
+        # Generate all combinations of parameter values
+        param_combinations = product(*parameter_grid.values())
+
+        # Iterate over each parameter combination
+        for params in param_combinations:
+            # Update parameters of the trainer
+            for param_name, param_value in zip(parameter_grid.keys(), params):
+                setattr(trainer.args, param_name, param_value)
+
+            # Train the model with the updated parameters
+            trainer.train()
+
+            # Evaluate the model on validation set
+            results = trainer.evaluate()
+
+            # Check if the current combination is the best
+            if results['eval_f1'] > best_score:
+                best_score = results['eval_f1']
+                best_params = params
+
+        # Set the best parameters to the trainer
+        for param_name, param_value in zip(parameter_grid.keys(), best_params):
+            setattr(trainer.args, param_name, param_value)
+
+        # Evaluate the model on the test set using the best parameters
+        test_results = trainer.evaluate(test_dataset)
+
+        return best_params, best_score, test_results
+
+    # results = trainer.evaluate(tokenized_dataset["test"])
+    # print(f"f1 score: {round(results['eval_f1'], 4)}")
+
+    tuner(trainer=trainer, parameter_grid=param_grid,test_dataset=tokenized_dataset["test"])
 
 
 if __name__ == "__main__":
