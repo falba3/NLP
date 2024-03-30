41811abded8ca6ade7c991f1cfe7150decb52a66
falba3 <99144557+falba3@users.noreply.github.com>
2024-03-30 08:05:41 +0100

    Mar 30 - tune roberta

--- transformers/tune_roberta.py
+++ transformers/tune_roberta.py
@@ -97,7 +97,7 @@ def main():
         "per_device_train_batch_size": [16],
         "per_device_eval_batch_size": [16],
         "num_train_epochs": [5],
-        "weight_decay": [0.01],
+        "weight_decay": [0.01, 0.001],
         # "fp16": [True, False],
         "load_best_model_at_end": [True],
         "metric_for_best_model": ["f1"]
